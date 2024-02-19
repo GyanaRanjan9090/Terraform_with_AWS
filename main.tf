2b3f8122047960f52d2b5197f11bf5a1224b82cd
GyanaRanjan9090 <gyanaswain09@gmail.com>
2024-02-19 22:38:26 +0530

    comment

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+#create with EC2 instance
 provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
@@ -7,7 +8,8 @@ resource "aws_instance" "example" {
     instance_type = "t2.micro"
     subnet_id = "subnet-0ef88a165e154d339"
     key_name = "my_own"
-    #key_name = ""
+   
+    
 
 
 }
