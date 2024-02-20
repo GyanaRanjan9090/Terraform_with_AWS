00c5f9477d23ab89e22033c6be96c916ac23ac45
GyanaRanjan9090 <gyanaswain09@gmail.com>
2024-02-20 14:38:49 +0530

    msg

--- main.tf
+++ main.tf
@@ -1,18 +1,4 @@
-variable "ami_value" {
-    description = "value for the ami"
-}
-variable "instance_type_value" {
-    description = " value for the instace "
-  
-}
-variable "subnet_id_value" {
-    description = "value for subnet"
-  
-}
-variable "key_name_velue" {
-    description = "value for keyname "
-  
-}
+
 provider "aws" {
     region = "us-east-1" 
   
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+ami_value = "ami-0c7217cdde317cfec"
+instance_type_value = "t2.micro"
+subnet_id_value = "subnet-0119ad56a4c9195e4"
+key_name_velue = "my_own"
